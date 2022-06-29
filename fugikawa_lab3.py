2a071711032488248b83b0b0d69266e89a1d97d0
efugikawa <elizabeth.fugikawa@gmail.com>
2022-06-29 17:23:05 -0600

    finish swp and lab 3

--- fugikawa_lab3.py
+++ fugikawa_lab3.py
@@ -1,4 +1,5 @@
 # RSA Cracking
+# Elizabeth Fugikawa, summer 2022
 import random
 from math import sqrt
 from time import time
@@ -15,25 +16,27 @@ def nBitPrime(n):
     primeFlag = False
     while primeFlag is False:
         randFloat = random.random()
-        numOut = randFloat * (2**n)
+        numOut = int(randFloat * (2**n))
         if numOut >= 2 and isPrime(numOut):
-            return int(numOut)
-        else:
-            return nBitPrime(n)
+            primeFlag = True
+            return numOut
 
 
 def factor(pq):
-    for i in range(int(sqrt(pq)), 2, -1):
-        if pq % i == 0:
+    for i in range(2, pq):
+        if pq % i == 0 and isPrime(i):
             p = i
             q = int(pq / p)
             return p, q
 
 
-for n in range(15, 19):
+for n in range(15, 33):
     pq = nBitPrime(n) * nBitPrime(n)
     time1 = time()
     p, q = factor(pq)
     time2 = time()
     t = (time2 - time1) * 1000
     print(n, t, p, q, pq)
+
+#produced trend line y = 8e-05 e^(0.6389x)
+#to crack a 1024-bit key, it would take 1.08e+280 milliseconds or 3.42e+269 years
--- fugikawa_swp.py
+++ fugikawa_swp.py
@@ -1,92 +1,111 @@
 # Small World Phenomenon
 # Elizabeth Fugikawa, summer 2022
 
-def mergeSort(L):
-    if len(L) < 2:
-        return L[:]
-    else:
-        mid = len(L) // 2
-        Left = mergeSort(L[:mid])
-        Right = mergeSort(L[mid:])
-        return merge(Left, Right)
-
-
-def merge(A, B):
-    out = []
-    i = 0
-    j = 0
-    while i < len(A) and j < len(B):
-        if A[i] < B[j]:
-            out.append(A[i])
-            i += 1
-        else:
-            out.append(B[j])
-            j += 1
-    while i < len(A):
-        out.append(A[i])
-        i += 1
-    while j < len(B):
-        out.append(B[j])
-        j += 1
-    return out
-
-
-def getLargestVertex(edges):
-    '''
-    :param edges: a list of edges as a file
-    :return: the value of the largest vertex as an integer
-    '''
-    edgeListA, edgeListB = [], []
-    f = open(edges, "r")
-    for edge in f:
-        edge = edge.split(" ")
-        edgeListA.append(int(edge[0]))
-        edgeListB.append(int(edge[1]))
-    f.close()
-    edgeListA = mergeSort(edgeListA)
-    edgeListB = mergeSort(edgeListB)
-    if edgeListA[-1] >= edgeListB[-1]:
-        return edgeListA[-1]
-    else:
-        return edgeListB[-1]
+import math
 
 
 def loadGraph(edgeFilename):
-    '''
+    """
     :param edgeFilename: string of filename of edges data
     :return: adjacency list representation of the undirected graph
-    '''
-    largestVertex = getLargestVertex(edgeFilename)
-    adjList = [[] for v in range(largestVertex + 1)]
-    f = open(edgeFilename, "r")
-    for edgeline in f:
-        edgeline = edgeline.split(" ")
-        vert_a = int(edgeline[0])
-        vert_b = int(edgeline[1])
-        adjList[vert_a].append(vert_b)
-        adjList[vert_b].append(vert_a)
-    f.close()
-    print("Graph loaded.")
+    """
+    with open(edgeFilename, "r") as f:
+        largestVertex = max([int(elem) for elem in f.read().split()])
+        adjList = [[] for v in range(largestVertex + 1)]
+    with open(edgeFilename, "r") as f:
+        for edgeline in f:
+            edgeline = edgeline.split(" ")
+            vert_a = int(edgeline[0])
+            vert_b = int(edgeline[1])
+            adjList[vert_a].append(vert_b)
+            adjList[vert_b].append(vert_a)
+    return adjList
 
 
 class MyQueue:
     def __init__(self):
-
-    def __repr__(self):
-        return f"MyQueue()"
+        self.queue = []
 
     def __str__(self):
+        return f"MyQueue({self.queue})"
 
-    def enqueue(self):
+    def enqueue(self, item):
+        return self.queue.insert(0, item)
 
     def dequeue(self):
+        if not self.empty():
+            return self.queue.pop()
+        else:
+            print("Error. Queue is empty")
 
     def empty(self):
+        return self.queue == []
+
+
+def bfs(G, s):
+    """
+    run a breadth-first search on the graph G
+    :param G: undirected graph as an adjacency list
+    :param s: source vertex
+    :return: list that contains the distance from s to every other vertex in G
+    """
+    Q = MyQueue()
+
+    dists = [math.inf for u in range(len(G))]
+    dists[s] = 0
+
+    Q.enqueue(s)
+
+    while not Q.empty():
+        u = Q.dequeue()
+        for v in G[u]:
+            if dists[v] == math.inf:
+                dists[v] = dists[u] + 1
+                Q.enqueue(v)
+
+    return dists
+
+
+def distanceDistribution(G):
+    """
+    computes distribution of all distances in G
+    :param G: an undirected graph as an adjacency list
+    :return: a dictionary that maps positive distances to their frequency
+    """
+    # list of lists of distances from each node to all others
+    distLists = [bfs(G, G.index(s)) for s in G]
+    distDict = dict()
+
+    for dList in distLists:
+        # need to remove the infinities (i.e. unconnected)
+        try:
+            distDict[math.inf] += dList.count(math.inf)
+        except:
+            distDict[math.inf] = dList.count(math.inf)
+        for dist in range(len(dList)):
+            try:
+                distDict[dist] += dList.count(dist)
+            except:
+                distDict[dist] = dList.count(dist)
+    distribDict = {k: str(round(v / sum(distDict.values()) * 100, 2)) + "%" for k, v in distDict.items()}
+    del distribDict[math.inf]
+
+    return distribDict
 
 
 def main():
-    loadGraph("edges.txt")
+    # gshort = loadGraph("edgesshort.txt")
+    # print(distanceDistribution(gshort))
+    g = loadGraph("edges.txt")
+    print(distanceDistribution(g))
 
 
 if __name__ == '__main__':
     main()
+
+'''
+output: {0: '0.02%', 1: '1.08%', 2: '16.65%', 3: '24.41%', 4: '35.93%', 5: '15.72%', 6: '4.15%', 7: '1.93%', 8: '0.1%'}
+
+what this shows is that the majority of nodes are less than 6 edges apart, which does fit with 
+the small world phenomenon that pairs are at most 6 connections away
+'''
